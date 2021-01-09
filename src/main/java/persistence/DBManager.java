6ccf153ef1d93708602866987324c42976a69c62
Domenico Panetta <73330859+TheDD97@users.noreply.github.com>
2021-01-09 16:41:24 +0100

    new beginning

---
+++ src/main/java/persistence/DBManager.java
@@ -0,0 +1,31 @@
+package persistence;
+
+public class DBManager {
+    private static DBManager instance = null;
+    static DBSource dataSource;
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver");
+            dataSource = new DBSource("jdbc:postgresql://localhost:5432/nomeDB", "postgres", "postgres");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static DBManager getInstance() {
+        if (instance == null) {
+            instance = new DBManager();
+        }
+        return instance;
+    }
+
+    private DBManager() {
+    }
+
+    public static DBSource getDataSource() {
+        return dataSource;
+    }
+
+
+}
