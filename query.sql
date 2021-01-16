8aa180e17e0bbb1c12604769560b6f01a43d9178
AlexFazio64 <alexfazio64@gmail.com>
2021-01-16 12:34:20 +0100

    Updated DB Structure

--- query.sql
+++ query.sql
@@ -8,46 +8,129 @@ SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
-SET default_tablespace = '';
+
 SET default_table_access_method = heap;
-CREATE TABLE public."ListaGiochi" (
- "emailUtente" character varying(255)[],
- "idGioco" character varying(255)[]
+
+--
+-- Name: recensione; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.recensione (
+    id integer NOT NULL,
+    valutazione integer NOT NULL,
+    testo character varying(255) NOT NULL,
+    "idGioco" character varying(255) NOT NULL,
+    "scrittaDa" character varying(320) NOT NULL
 );
-ALTER TABLE public."ListaGiochi" OWNER TO postgres;
-CREATE TABLE public."Profilo" (
- username character varying(50)[] NOT NULL,
- descrizione character varying(255)[],
- email character varying(255)[] NOT NULL,
- immagine bytea[],
- password character varying(20)[] NOT NULL
-);
-ALTER TABLE public."Profilo" OWNER TO postgres;
-CREATE TABLE public."Recensione" (
- id integer NOT NULL,
- valutazione integer NOT NULL,
- testo character varying(255)[] NOT NULL,
- "idGioco" character varying(255)[] NOT NULL,
- "scrittaDa" character varying(255)[] NOT NULL
-);
-ALTER TABLE public."Recensione" OWNER TO postgres;
+
+
+--
+-- Name: Recensione_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
 CREATE SEQUENCE public."Recensione_id_seq"
- AS integer
- START WITH 1
- INCREMENT BY 1
- NO MINVALUE
- NO MAXVALUE
- CACHE 1;
- ALTER TABLE public."Recensione_id_seq" OWNER TO postgres;
-ALTER SEQUENCE public."Recensione_id_seq" OWNED BY public."Recensione".id;
-ALTER TABLE ONLY public."Recensione" ALTER COLUMN id SET DEFAULT nextval('public."Recensione_id_seq"'::regclass);
-SELECT pg_catalog.setval('public."Recensione_id_seq"', 2, true);
-ALTER TABLE ONLY public."Profilo"
- ADD CONSTRAINT "Profilo_pkey" PRIMARY KEY (email);
-ALTER TABLE ONLY public."Recensione"
- ADD CONSTRAINT "Recensione_pkey" PRIMARY KEY (id);
-ALTER TABLE ONLY public."ListaGiochi"
- ADD CONSTRAINT "ListaGiochi_emailUtente_fkey" FOREIGN KEY ("emailUtente") REFERENCES public."Profilo"(email) ON UPDATE
-CASCADE ON DELETE CASCADE;
-ALTER TABLE ONLY public."Recensione"
- ADD CONSTRAINT "Recensione_emailProfilo_fkey" FOREIGN KEY ("scrittaDa") REFERENCES public."Profilo"(email);
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: Recensione_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public."Recensione_id_seq" OWNED BY public.recensione.id;
+
+
+--
+-- Name: listagiochi; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.listagiochi (
+    "emailUtente" character varying(320) NOT NULL,
+    "idGioco" character varying(255) NOT NULL
+);
+
+
+--
+-- Name: profilo; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.profilo (
+    email character varying(320) NOT NULL,
+    password character varying(64) NOT NULL,
+    username character varying(25) NOT NULL,
+    descrizione character varying(255),
+    immagine bytea
+);
+
+
+--
+-- Name: recensione id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.recensione ALTER COLUMN id SET DEFAULT nextval('public."Recensione_id_seq"'::regclass);
+
+
+--
+-- Name: profilo Profilo_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.profilo
+    ADD CONSTRAINT "Profilo_pkey" PRIMARY KEY (email);
+
+
+--
+-- Name: recensione Recensione_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.recensione
+    ADD CONSTRAINT "Recensione_pkey" PRIMARY KEY (id);
+
+
+--
+-- Name: listagiochi email-gioco; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.listagiochi
+    ADD CONSTRAINT "email-gioco" PRIMARY KEY ("emailUtente", "idGioco");
+
+
+--
+-- Name: profilo username; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.profilo
+    ADD CONSTRAINT username UNIQUE (username);
+
+
+--
+-- Name: fki_emailFK; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX "fki_emailFK" ON public.listagiochi USING btree ("emailUtente");
+
+
+--
+-- Name: fki_profiloFK; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX "fki_profiloFK" ON public.recensione USING btree ("scrittaDa");
+
+
+--
+-- Name: listagiochi emailFK; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.listagiochi
+    ADD CONSTRAINT "emailFK" FOREIGN KEY ("emailUtente") REFERENCES public.profilo(email) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: recensione profiloFK; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.recensione
+    ADD CONSTRAINT "profiloFK" FOREIGN KEY ("scrittaDa") REFERENCES public.profilo(email) ON UPDATE CASCADE ON DELETE CASCADE;
