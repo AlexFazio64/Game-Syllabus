0dfc81cf9d5f9968915d8c1633303dadc2f645a4
Your Name <pntdnc97h28d976l@studenti.unical.it>
2021-01-20 22:25:30 +0100

    added platformsStyle.css, platforms.jsp, platformsScript and platformsContoller

---
+++ src/main/java/group64/gamesyllabus/controller/PlatformsController.java
@@ -0,0 +1,18 @@
+package group64.gamesyllabus.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@org.springframework.stereotype.Controller
+
+
+public class PlatformsController {
+
+    @GetMapping("/platforms")
+    public String genreChoose(@RequestParam String start, Model model) {
+        model.addAttribute("start", start);
+        return "platforms";
+    }
+}
