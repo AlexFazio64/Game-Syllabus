1efee8219cc5f78b5831f9dd47cd0ec220dde841
AlexFazio64 <alexfazio64@gmail.com>
2021-01-21 18:15:41 +0100

    Spero veramente di non aver perso niente
    altrimenti sparo prima a daniele e poi a me

--- src/main/java/persistence/DAO/ProfiloDAO.java
+++ src/main/java/persistence/DAO/ProfiloDAO.java
@@ -5,13 +5,13 @@ import Model.Profilo;
 import java.util.List;
 
 public interface ProfiloDAO {
-
+    
     boolean save(Profilo profilo); // CREATE
     Profilo findByPrimaryKey(String email); //RETRIEVE
     Profilo findByUsername(String username); //NEEDED
     List<Profilo> findAll();
-    void update (Profilo profilo); // UPDATE
-    void delete(String emailUtente); //DELETE
+    boolean update (Profilo profilo); // UPDATE
+    boolean delete(String emailUtente); //DELETE
 
 
 }
