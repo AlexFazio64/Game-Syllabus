93ff890abbc715a4027e34621fc0f88a4c8f7b7e
Alessandro Fazio <alexfazio64@gmail.com>
2021-01-25 03:42:10 +0100

    Account editing w/ review deletion and update working. Added ugly backgrounds...

--- src/main/java/Model/Recensione.java
+++ src/main/java/Model/Recensione.java
@@ -1,49 +1,54 @@
 package Model;
 
 public class Recensione {
-    private Integer id;
-    private Integer valutazione;
-    private String testo;
-    private String idGioco;
-    private String scrittaDa;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Integer getValutazione() {
-        return valutazione;
-    }
-
-    public void setValutazione(Integer valutazione) {
-        this.valutazione = valutazione;
-    }
-
-    public String getTesto() {
-        return testo;
-    }
-
-    public void setTesto(String testo) {
-        this.testo = testo;
-    }
-
-    public String getIdGioco() {
-        return idGioco;
-    }
-
-    public void setIdGioco(String idGioco) {
-        this.idGioco = idGioco;
-    }
-
-    public String getScrittaDa() {
-        return scrittaDa;
-    }
-
-    public void setScrittoDa(String scrittaDa) {
-        this.scrittaDa = scrittaDa;
-    }
+	private Integer id;
+	private Integer valutazione;
+	private String testo;
+	private String idGioco;
+	private String scrittaDa;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public Integer getValutazione() {
+		return valutazione;
+	}
+	
+	public void setValutazione(Integer valutazione) {
+		this.valutazione = valutazione;
+	}
+	
+	public String getTesto() {
+		return testo;
+	}
+	
+	public void setTesto(String testo) {
+		this.testo = testo;
+	}
+	
+	public String getIdGioco() {
+		return idGioco;
+	}
+	
+	public void setIdGioco(String idGioco) {
+		this.idGioco = idGioco;
+	}
+	
+	public String getScrittaDa() {
+		return scrittaDa;
+	}
+	
+	public void setScrittoDa(String scrittaDa) {
+		this.scrittaDa = scrittaDa;
+	}
+	
+	@Override
+	public String toString() {
+		return "Recensione{" + "id=" + id + ", valutazione=" + valutazione + ", testo" + "='" + testo + '\'' + ", idGioco='" + idGioco + '\'' + ", " + "scrittaDa='" + scrittaDa + '\'' + "}<br>";
+	}
 }
--- src/main/java/group64/gamesyllabus/controller/AccountController.java
+++ src/main/java/group64/gamesyllabus/controller/AccountController.java
@@ -1,6 +1,7 @@
 package group64.gamesyllabus.controller;
 
 import Model.Profilo;
+import Model.Recensione;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,12 +9,18 @@ import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import persistence.DAO.JDBC.ProfiloDAOPG;
+import persistence.DAO.JDBC.RecensioneDAOPG;
 
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @Controller
 public class AccountController {
+	
+	private boolean isNotLogged(String username, HttpSession session) {
+		return session.getAttribute("usernameLogged") == null || !session.getAttribute("usernameLogged").toString().equals(username);
+	}
 	
 	@GetMapping(path = "/account/{username}")
 	public String getAccount(@PathVariable("username") String username, Model model, HttpSession session) {
@@ -21,11 +28,21 @@ public class AccountController {
 			return "redirect:/";
 		}
 		
+		RecensioneDAOPG recdaopg = new RecensioneDAOPG();
+		ArrayList<Recensione> rec = recdaopg.findAllByEmail(session.getAttribute("emailLogged").toString());
+		if ( rec.size() > 0 ) {
+			StringBuilder ids = new StringBuilder();
+			rec.forEach(e -> ids.append(e.getIdGioco()).append(","));
+			ids.deleteCharAt(ids.lastIndexOf(","));
+			model.addAttribute("ids", ids.toString());
+		}
+		model.addAttribute("reviews", rec);
 		ProfiloDAOPG daopg = new ProfiloDAOPG();
 		Profilo profilo = daopg.findByUsername(username);
 		
 		model.addAttribute("password", profilo.getPassword());
 		model.addAttribute("username", profilo.getUsername());
+		
 		String descrizione = profilo.getDescrizione();
 		model.addAttribute("descrizione", descrizione == null || descrizione.equals("") ? null : descrizione);
 		
@@ -36,14 +53,13 @@ public class AccountController {
 		return "profile";
 	}
 	
-	private boolean isNotLogged(String username, HttpSession session) {
-		return session.getAttribute("usernameLogged") == null || !session.getAttribute("usernameLogged").toString().equals(username);
-	}
-	
-	@PostMapping("/account/edit")
-	public String editAccount(HttpSession session, @RequestParam String password, @RequestParam String new_password, @RequestParam String username, @RequestParam String descrizione, @RequestParam MultipartFile image, Model model) {
+	@PostMapping("/account/{username}/edit")
+	public String editAccount(HttpSession session, @RequestParam String password, @RequestParam String new_password, @PathVariable("username") String usr, @RequestParam String username, @RequestParam String descrizione, @RequestParam MultipartFile image, Model model) {
+		if ( isNotLogged(usr, session) ) {
+			return "redirect:/";
+		}
+		
 		ProfiloDAOPG daopg = new ProfiloDAOPG();
-		String usr = session.getAttribute("usernameLogged").toString();
 		if ( !password.equals("") ) {
 			if ( !BCrypt.checkpw(password, daopg.findByUsername(usr).getPassword()) ) {
 				//password is wrong
@@ -85,14 +101,42 @@ public class AccountController {
 		return "redirect:/account/" + username;
 	}
 	
-	@DeleteMapping("/account/delete")
+	@DeleteMapping("/account/{username}/delete")
 	@ResponseBody
-	public String delete(HttpSession session, @RequestHeader String username) {
+	public String deleteAccount(HttpSession session, @RequestHeader String username) {
 		if ( isNotLogged(username, session) ) {
 			return "/";
 		}
 		ProfiloDAOPG daopg = new ProfiloDAOPG();
 		daopg.delete(session.getAttribute("emailLogged").toString());
 		return "/doLogout";
+	}
+	
+	@PostMapping("/account/{username}/review/{id}/edit")
+	public String editReview(@PathVariable("username") String usr, @PathVariable("id") int id, HttpSession session, @RequestParam String text, @RequestParam int stars) {
+		if ( isNotLogged(usr, session) ) {
+			return "redirect:/";
+		}
+		
+		RecensioneDAOPG daopg = new RecensioneDAOPG();
+		Recensione new_rec = new Recensione();
+		new_rec.setId(id);
+		new_rec.setTesto(text == null || text.equals("") ? null : text);
+		new_rec.setValutazione(stars);
+		daopg.update(new_rec);
+		return "redirect:/account/" + usr;
+	}
+	
+	@DeleteMapping("/account/{username}/review/{id}/delete")
+	@ResponseBody
+	public String deleteReview(HttpSession session, @PathVariable("username") String username, @PathVariable("id") int id) {
+		if ( isNotLogged(username, session) ) {
+			return "/";
+		}
+		RecensioneDAOPG daopg = new RecensioneDAOPG();
+		Recensione del = new Recensione();
+		del.setId(id);
+		daopg.delete(del);
+		return "/account/" + username;
 	}
 }
